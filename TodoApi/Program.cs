6c5fbbcb132126328e3a3ffb35edd46be4049142
iamkacper <Kacper.Pys@microsoft.wsei.edu.pl>
2022-07-22 16:15:41 +0200

    .

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
+
 //builder.Services.AddSwaggerGen(c =>
 //{
 //    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
@@ -17,11 +18,14 @@ builder.Services.AddDbContext<TodoContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+
     //    app.UseSwagger();
     //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+
 }
 
 app.UseHttpsRedirection();
