2cf5fb329d94042b3cf12f67358ff1c37fcbf88c
iamkacper <Kacper.Pys@microsoft.wsei.edu.pl>
2022-07-22 16:20:07 +0200

    Add sth. (not important)

--- TodoApi/Models/PaymentDTO.cs
+++ TodoApi/Models/PaymentDTO.cs
@@ -13,7 +13,7 @@ namespace TodoApi.Models
         [Range(100, 999, ErrorMessage = "Please enter a valid CVV.")]
         public string? CardSecurityCode { get; set; }   
 
-        [Range((0.1), 100, ErrorMessage = "Price must be between $0.1 and $100")]
+        [Range((0.1), 100, ErrorMessage = "Price must be between €0.1 and €100")]
         public string? Amount { get; set; }    
 
         public string? Currency { get; set; }
